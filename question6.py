b1d1677caf3d2288690bf10da0b513a85afb9251
Billy Daniel W <danieldany068@gmail.com>
2026-08-10 20:02:15 +0530

    question 6 completed with above and below average

--- question6.py
+++ question6.py
@@ -9,14 +9,22 @@ minimum = min(expenses)
 maximum = max(expenses)
 above = 0
 below = 0
+av_above = 0
+av_below = 0
 for i in expenses:
     if i >500:
         above += 1
     else:
         below += 1
+    if i > average :
+        av_above +=1
+    else:
+        av_below +=1
     print(f"Total Expenses: {total}")
     print(f"Average Expenses: {average:.2f}")
     print(f"Highest Expenses: {maximum}")
     print(f"Lowest Expense: {minimum}")
     print(f"Number of Expenses above ₹500: {above}")
     print(f"Number of expenses Below or Equal to ₹500: {below}")
+    print(f"Number of expenses above average: {av_above}")
+    print(f"Number of expenses Below average: {av_below}")
