9721659afa8dea23fd012893886df7fa35d6a9ca
Billy Daniel W <danieldany068@gmail.com>
2026-08-10 21:09:40 +0530

    question 7 completed after solving the error with second 10

--- question7.py
+++ question7.py
@@ -1,7 +1,7 @@
 values = [10,10,20,20,20,30,10,10,40]
 new_values = []
-for i in values:
-    if i not in new_values:
-        new_values.append(i)
+for i in range(len(values)):
+    if values[i]!= values[i-1] :
+        new_values.append(values[i])
 print(f"Original List: {values}")
 print(f"Result: {new_values}")
