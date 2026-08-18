23fba8b987c95a0a34e7aa4261e3c3ceeebb1ff8
Billy Daniel W <danieldany068@gmail.com>
2026-08-18 14:53:19 +0530

    even number with and without function-18-08

---
+++ Practice/problem1/src/even_number_fuction.py
@@ -0,0 +1,6 @@
+def even_number():
+    for i in range(101):
+        if i%2 == 0:
+           print(i)
+print("Even Numbers From 0 to 100")
+even_number()
---
+++ Practice/problem1/src/even_numbers.py
@@ -0,0 +1,5 @@
+#Even numbers
+print("Even Numbers From 0 to 100")
+for i in range(101):
+    if i%2 == 0:
+        print(i)
